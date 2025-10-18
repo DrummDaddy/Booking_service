116febd46a81259c8a33a6ebfdcd42c7e5d90682
DrummDaddy <alexpospelov87@gmail.com>
2025-10-18 14:33:53 +0700

    добавил модели

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Booking_service
+
+go 1.25.0
+
+require go.mongodb.org/mongo-driver v1.17.4 // indirect
---
+++ internal/models/models.go
@@ -0,0 +1,70 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type BookingStatus string
+
+const (
+	BookingStatusReserved  BookingStatus = "reserved"
+	BookingStatusPending   BookingStatus = "pending"
+	BookingStatusConfirmed BookingStatus = "confirmed"
+	BookingStatusCancelled BookingStatus = "cancelled"
+	BookingStatusExpired   BookingStatus = "expired"
+)
+
+type Booking struct {
+	ID      primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	USerID  primitive.ObjectID `json:"user_id" bson:"user_id"`
+	EventID primitive.ObjectID `json:"event_id" bson:"event_id"`
+	Status  BookingStatus      `json:"status" bson:"status"`
+	Tickets []BookingTicket    `json:"tickets" bson:"tickets"`
+
+	Subtotal    float64 `json:"subtotal" bson:"subtotal"`
+	ServiceFree float64 `json:"service_free" bson:"service_free"`
+	TotalAmount float64 `json:"total_amount" bson:"total_amount"`
+	Currency    string  `json:"currency" bson:"currency"`
+
+	ReservedUntil time.Time `json:"reserved_until" bson:"reserved_until"`
+
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
+}
+
+type BookingTicket struct {
+	TicketTypeID   primitive.ObjectID `json:"ticket_type_id" bson:"ticket_type_id"`
+	TicketTypeName string             `json:"ticket_type_name" bson:"ticket_type_name"`
+	Quantity       int                `json:"quantity" bson:"quantity"`
+	UnitPrice      float64            `json:"unit_price" bson:"unit_price"`
+	TotalPrice     float64            `json:"total_price" bson:"total_price"`
+	Seats          []Seat             `json:"seats,omitempty" bson:"seats,omitempty"`
+}
+
+type Seat struct {
+	Sector string `json:"sector" bson:"sector"`
+	Row    string `json:"row" bson:"row"`
+	Number string `json:"number" bson:"number"`
+	SeatID string `json:"seat_id" bson:"seat_id"`
+}
+
+type BookingRequest struct {
+	EventID string            `json:"event_id"`
+	Tickets []TicketSelection `json:"tickets"`
+	UserID  string            `json:"-"`
+}
+
+type TicketSelection struct {
+	TicketID string `json:"ticket_id"`
+	Quantity int    `json:"quantity"`
+	Seats    []Seat `json:"seats,omitempty"`
+}
+
+type BookingResponse struct {
+	BookingID     string          `json:"booking_id"`
+	Status        BookingStatus   `json:"status"`
+	ReservedUntil time.Time       `json:"reserved_until"`
+	TotalAmount   float64         `json:"total_amount"`
+	Tickets       []BookingTicket `json:"tickets"`
+}
